6173bb7865db3c83e4e38c98d2d57a11f520fdfa
Aron Raes <aron.raes@student.ehb.be>
2018-04-18 15:21:27 +0200

    test

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aron_Raes_S2IT_werkstuk1
+//
+//  Created by Aron Raes on 18/04/18.
+//  Copyright © 2018 Aron Raes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

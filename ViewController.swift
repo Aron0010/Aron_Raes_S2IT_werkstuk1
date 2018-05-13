bacc28caf629d737238d1f5a8610cff2a23013d1
Aron Raes <aron.raes@student.ehb.be>
2018-05-13 17:54:27 +0200

    Laatste aanpassingen

--- ViewController.swift
+++ ViewController.swift
@@ -12,11 +12,11 @@ import MapKit
 class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var myMapView: MKMapView!
-    
+    //locaties in Groot Bijgaarden.
     var locations = [
-        ["title": "Delhaize",    "latitude": 50.872762, "longitude": 4.248058],
-        ["title": "Aldi ",   "latitude": 50.870849, "longitude": 4.252981],
-        ["title": "Kerk ",   "latitude": 50.871028, "longitude": 4.265655]
+        ["title": "Jos",    "latitude": 50.872762, "longitude": 4.248058],
+        ["title": "Jef ",   "latitude": 50.870849, "longitude": 4.252981],
+        ["title": "Mark ",   "latitude": 50.871028, "longitude": 4.265655]
     ]
     
       var locationManager = CLLocationManager()
@@ -24,8 +24,33 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        //zoomen naar eigen locatie link: https://stackoverflow.com/questions/41189147/mapkit-zoom-to-user-current-location?answertab=active#tab-top
+        myMapView.delegate = self
+        myMapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self as? CLLocationManagerDelegate
+        
+        //Check for Location Services
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self as? CLLocationManagerDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+        //Zoom to user location
+        let noLocation = CLLocationCoordinate2D()
+        let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 200, 200)
+        myMapView.setRegion(viewRegion, animated: false)
+        
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
+        
         
         for location in locations {
             let annotation = MKPointAnnotation()
@@ -34,11 +59,6 @@ class ViewController: UIViewController, MKMapViewDelegate {
             self.myMapView.addAnnotation(annotation)
             self.myMapView.showAnnotations(self.myMapView.annotations, animated: true)
         }
-        
-        
-        
-        
-        
       
         
     }
